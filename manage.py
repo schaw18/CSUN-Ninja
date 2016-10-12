e764a78b530dd8842450aaa642dd0884319d7e41
sjweagley <scott.weagley.894@my.csun.edu>
2016-10-11 19:37:00 -0700

    This is a second test.

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ninja_490.settings")
     #Test from Scott
+	#Second test from Scott
     try:
         from django.core.management import execute_from_command_line
     except ImportError:

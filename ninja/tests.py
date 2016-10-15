8edde3086a5e63871c0fb32a0a70415db6cb4f47
Valera <valery.maniuk.55@my.csun.edu>
2016-10-14 19:07:01 -0700

    whatever was fetched from the remote needs to be merged

--- ninja/tests.py
+++ ninja/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from .views import *
+from .models import *
 
-# Create your tests here.
+
+class ListOfCourses(TestCase):
+    """creates 6 (SIX) employees
+        then counts waht beeing passed from the template"""
+
+    def setUp(self):
+        # creating courses
+        Course.objects.create(subject = "COMP", course_level = "100")
+        Course.objects.create(subject = "COMP", course_level = "200")
+        Course.objects.create(subject = "COMP", course_level = "300")
+        Course.objects.create(subject = "COMP", course_level = "400")
+        Course.objects.create(subject = "ART", course_level = "400")
+        Course.objects.create(subject = "ART", course_level = "500")
+
+        self.factory = RequestFactory()
+
+
+
+    def test_count_couses(self):
+
+        request = self.factory.get('/', {})
+        print(ShowAll(request))

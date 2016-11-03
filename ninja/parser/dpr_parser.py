251c5fb17f5810249e23a74fd3d9f91daa8557cc
levsukharev <lev.sukharev.666@my.csun.edu>
2016-11-02 20:05:10 -0700

    git now ignores dpr_output.txt, working on parsing course list

--- ninja/parser/dpr_parser.py
+++ ninja/parser/dpr_parser.py
@@ -1,3 +1,4 @@
+import PyPDF2
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
@@ -30,32 +31,42 @@ def main():
         device.close()
         retstr.close()
 
-        get_subjects(str)
         return str
 
+    def pattern_search(dpr_text):
+        #course_pattern = re.compile(r'(?:\n| +)([A-Z]{2,4}|[A-Z]{1,2} [A-Z])(?: +)(\d{3}(?:[A-Z]| )?(?:,|\n|,\n))+')
+        #course_pattern = re.compile(r'''
+        #(?:\n| +)           # New line or spaces precede course name
+        #([A-Z]{2,4}         # Course name can be 2-4 letters
+        #|[A-Z]{1,2} [A-Z])  # or 1-2 letters followed by 1 letter
+        #(?: +)              # Course name followed by at least 1 space
+        #\d{3}              # Course number must be 3 digits
+        #?:[A-Z]| )?        # Followed by an optional letter or space
+        #?:,|\n|,\n)        # but must end with , or new line or both
+        #)+                  # and can repeat more than once
+        #''', re.VERBOSE)
 
-    def get_subjects(dpr_text):
-        pattern1 = re.compile(r'(GE.*:)')
-        pattern2 = re.compile(r'\s')
-        result = re.findall(pattern2, dpr_text)
-        print (result)
-        #return result
+        ge_pattern = re.compile(r'(?:GE(?:[A-Z]| | &)*:)(?:.|\n)*(?:GE(?:[A-Z]| | &)*:)')
 
+        print(re.findall(ge_pattern, dpr_text))
 
 #==============================================================================#
     logging.debug('START File Parsing')
 
+    # This is a path to the source PDF file
     DPR_FILE_NAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'SampleDPR.pdf')
-
-    # This is a path to the txt file containing all subject abbreviations
-    #SUBJECTS_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'course_subjects.txt')
 
     # get a raw text from the pdf
     text_source = pdf_to_txt(DPR_FILE_NAME)
 
-    output_text = open('dpr_output.txt', 'wb')
-    output_text.write(text_source.encode('utf-8'))
-    output_text.close()
+    # begin pattern matching
+    pattern_search(text_source)
+
+    # output to file (for debugging purposes)
+    #output_text = open('dpr_output.txt', 'w')
+    #output_text.write(text_source)
+    #output_text.close()
+
 
 if __name__ == '__main__':
     main()

7921df49af8ddd003c04082f209cd66afd637184
scott weagley <scott.weagley.894@my.csun.edu>
2017-04-12 12:50:17 -0700

    Modified forms to reflect the changes in form fields for signup

--- ninja/forms.py
+++ ninja/forms.py
@@ -2,23 +2,27 @@ from django import forms
 from .models import UserFilters
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+	username = forms.CharField()
+	password = forms.CharField(widget=forms.PasswordInput)
 
 class SignUpForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    password_repeat = forms.CharField(widget=forms.PasswordInput)
-    email = forms.EmailField(required=False)
+	first_name = forms.CharField()
+	last_name = forms.CharField()
+	username = forms.CharField()
+	student_id = forms.CharField()
+	password = forms.CharField(widget=forms.PasswordInput)
+	password_repeat = forms.CharField(widget=forms.PasswordInput)
+	#email = forms.EmailField(required=False)
+
 
 class FilterForm(forms.ModelForm):
-    # Time fiters
-    class Meta:
-        model = UserFilters
-        exclude =['user']
+	# Time fiters
+	class Meta:
+		model = UserFilters
+		exclude =['user']
 
 class DPRUploadForm(forms.Form):
-    docfile = forms.FileField(
-        label='Select you DPR file',
-        help_text='but don\'t tell anyone'
-    )
+	docfile = forms.FileField(
+		label='Select you DPR file',
+		help_text='but don\'t tell anyone'
+	)
